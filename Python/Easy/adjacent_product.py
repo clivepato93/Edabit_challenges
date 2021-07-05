3a423238d278166d7a0b32e45178904d831559c6
clivepato93 <clivetoniato1@hotmail.com>
2021-07-05 17:18:34 +0100

    more testing

--- Python/Easy/adjacent_product.py
+++ Python/Easy/adjacent_product.py
@@ -2,17 +2,34 @@
 # command to run in terminal
 # python3 -m doctest -v adjacent_product.py
 
+'''
+>>> adjacent_product(['e','i'])
+Traceback (most recent call last):
+        ...
+TypeError: can't multiply sequence by non-int of type 'str'
+'''
+
 
 def adjacent_product(lst):
-    """
+    """ returns the highest product in a list
+
     >>> adjacent_product([3,6,-2,-5,7,3])
     21
 
     >>> adjacent_product([5,6,-4,2,3,2,-23])
     30
+
+    >>> adjacent_product([])
+    Traceback (most recent call last):
+        ...
+    ValueError: max() arg is an empty sequence
+
+    >>> adjacent_product(['e','i'])
+    Traceback (most recent call last):
+        ...
+    TypeError: can't multiply sequence by non-int of type 'str'
     """
     return max((a*b) for a,b in zip(lst,lst[1:]))
-
 
 print(adjacent_product([3, 6, -2, -5, 7, 3]), 21)
 print(adjacent_product([5, 6, -4, 2, 3, 2, -23]) , 30)
